d7fe2e7269074468c3c866856bb292c81f3eac50
RichardF <r.fa@live.ca>
2019-09-21 10:54:09 -0400

    Test

--- ElevateTechJam/ElevateTechJam/ViewController.swift
+++ ElevateTechJam/ElevateTechJam/ViewController.swift
@@ -5,13 +5,14 @@
 //  Created by Kunal Kumar on 2019-09-21.
 //  Copyright © 2019 TD. All rights reserved.
 //
-
+ 
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         
         let vm = DeviceViewModel()
